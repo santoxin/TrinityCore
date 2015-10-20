67e42c1a235354e3fa05549e751b5e2f6fcd0cf8
r00ty-tc <r00ty-tc@users.noreply.github.com>
2015-10-20 01:35:35 +0100

    Dynamic Creature/Go spawning:
    * Don't load objects during grid load, if they have a respawn timer
    * Load from respawn DB into grid respawn lists
    * re-write Creature Respawn() function to work with dynamic spawns
    * Create separate function to respawn from list
    * Created helper functions to remove/respawn based on spawnid, gridid,
      cell/area/zone
    * re-write .respawn command to use new functionality.
    * Removed existing respawn removal functions in favour of new versions
      aside from "compatibility mode"
    * Compatibility mode added for dynamic spawns.
    * Extended .gobject info command with more useful data. GUID, location and
      distance.
    * Implement command to list respawns in the area or map, showing original
      time and adjusted respawn time.
    
    Rewrite of storage mechanism for respawns:
    
    * Now uses 3 maps, for cell/zone/area + grid + spawnID for multiple lookups
    * Single respawnInfo object and pointers in each map to the same entry
    * Helper functions to ensure no dead pointers, no storage left unattached
    etc.
    * A compatibility mode added for creatures for interim use until BG/BF
    and instances are converted.
    
    * temporary summons/non DB spawns will by default use the old spawn system
      with an option to use the new one. Meaning they need no changes to still
      function Meaning they need no changes to still function.
      Marked as "Compatibility mode"
    * Dynamic spawns will only be active by default on DB loaded creatures.
    
    Creature/Go group functionaity:
    * New tables added for group template/membership
    * Load creatures/gos into groups (default 0)
    * Default (system) groups 0-255, 256+ user groups (manual spawn only on
      user groups)
    * Honour flag for compatibility mode (don't use dynamic spawning if group
      has this flag set)
    * Fully implement compatibility mode for both creature and gos
    * Implement display for group and compatibility mode state for creature
      and go with npc info and gobject info commands
    * c++ functions to Spawn/Despawn manual creature/go groups
    * new commands gobject spawngroup/despawngroup and npc
      spawngroup/despawngroup to spawn/despawn manual groups for testing
    * SAI actions to spawn/despawn creature/go groups
    * Extended functionality to allow forced spawn, ignore respawn and remove
      respawn time on despawn. Functinality as follows:
     - Force spawn (will spawn all creatures/gameobjects in a group even if
       there are already active versions alive/active)
     - Ignore Repsawn (will spawn creatures in a group regardless of their
       respawn timer status)
     - removerespawntime (Despawn option, which will cause the respawn
       time to be removed when despawning group)
    
    Escort NPC changes:
    * Respawn timer starts when an escort quest is taken, not on creature
      death
    * Initial support for respawning replacement escort NPCs when user takes
      quest
    * General protection against respawning if any spawned instances of the
      spawnId are not currently on escort.
    * Limited to creatures with the correct escort NPC flag
    * Supports dynamic respawn rates
    
    Dynamic Respawn Rates:
    * Based on player within dynamic spawn scope. Cell (specified radius),
      Zone or Area.
    * Will decrease respawn time based on a factor multiplied by number of
      players within scope range. With minimum respawn time.
    * Optionally applied to Escort NPCs.
    
    Auto server restart:
    * Will queue a restart at a convenient time when one threshold is
      breached. With regular warnings and a 30 minute countdown when the time
      comes.
    * When a second threshold is reached an emergency 5 minute restart is
      queued.
    
    Misc changes:
    * Support for grid persistance (no grid unload for wintergrasp during
      battle for example)
    
    Adjust world DB commands, to match changed values in auth DB

--- src/server/game/AI/SmartScripts/SmartScriptMgr.h
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.h
@@ -583,8 +583,12 @@ enum SMART_ACTION
     SMART_ACTION_TRIGGER_RANDOM_TIMED_EVENT         = 125,    // id min range, id max range
     SMART_ACTION_REMOVE_ALL_GAMEOBJECTS             = 126,
     SMART_ACTION_STOP_MOTION                        = 127,	  // stopMoving, movementExpired
+    SMART_ACTION_SPAWN_CREATUREGROUP                = 128,    // Group ID, min secs, max secs
+    SMART_ACTION_DESPAWN_CREATUREGROUP              = 129,    // Group ID, min secs, max secs
+    SMART_ACTION_SPAWN_GAMEOBJECTGROUP              = 130,    // Group ID, min secs, max secs
+    SMART_ACTION_DESPAWN_GAMEOBJECTGROUP            = 131,    // Group ID, min secs, max secs
 
-    SMART_ACTION_END                                = 128
+    SMART_ACTION_END                                = 132
 };
 
 struct SmartAction
@@ -1108,6 +1112,13 @@ struct SmartAction
         {
             uint32 disable;
         } disableEvade;
+        struct
+        {
+            uint32 groupId;
+            uint32 minDelay;
+            uint32 maxDelay;
+            uint32 spawnflags;
+        } groupSpawn;
 
         struct
         {
@@ -1150,6 +1161,14 @@ struct SmartAction
             uint32 param6;
         } raw;
     };
+};
+
+enum SMARTAI_SPAWN_FLAGS
+{
+    SMARTAI_SPAWN_FLAG_NONE                 = 0x00,
+    SMARTAI_SPAWN_FLAG_IGNORE_RESPAWN       = 0x01,
+    SMARTAI_SPAWN_FLAG_FORCE_SPAWN          = 0x02,
+    SMARTAI_SPAWN_FLAG_NOSAVE_RESPAWN       = 0x04,
 };
 
 enum SMARTAI_TEMPLATE
